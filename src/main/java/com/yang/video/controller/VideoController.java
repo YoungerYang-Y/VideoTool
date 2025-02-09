40ec3b6f06eb367e73d5464ab5b2a9259c1e8bab
YangYang <1059376437@qq.com>
2025-02-09 21:47:37 +0800

    feat(upload): 优化文件上传功能并添加并发控制
    - 添加 Semaphore 以限制并发请求，提高服务器稳定性
    - 优化上传逻辑，处理异常情况并返回相应错误信息
    - 调整代码结构，提高可读性和可维护性

--- src/main/java/com/yang/video/controller/VideoController.java
+++ src/main/java/com/yang/video/controller/VideoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.concurrent.Semaphore;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -18,26 +20,45 @@ public class VideoController {
 
     private final VideoService videoService;
 
-    // 定义文件上传接口
+    // 限制最多 3 个并发请求
+    private final Semaphore semaphore = new Semaphore(3);
+
+    /**
+     * 文件上传
+     *
+     * @param file 上传的文件
+     * @return 文件名
+     */
     @PostMapping("/upload")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
 
-        log.debug("文件上传开始");
-
-        if (file.isEmpty()) {
-            log.warn("文件为空，请选择一个视频文件上传");
-            return ResponseEntity.badRequest().body("文件为空，请选择一个视频文件上传");
+        if (!semaphore.tryAcquire()) {
+            return ResponseEntity.badRequest().body("服务器繁忙，请稍后重试！");
         }
 
-        // 限制文件大小，文件超过100M时拒绝上传
-        if (file.getSize() > 1024 * 1024 * 100) {
-            log.warn("文件大小超过100M，请选择一个较小的视频文件上传");
-            return ResponseEntity.badRequest().body("文件大小超过100M，请选择一个较小的视频文件上传");
+        try {
+            log.debug("文件上传开始");
+
+            if (file.isEmpty()) {
+                log.warn("文件为空，请选择一个视频文件上传");
+                return ResponseEntity.badRequest().body("文件为空，请选择一个视频文件上传");
+            }
+
+            // 限制文件大小，文件超过100M时拒绝上传
+            if (file.getSize() > 1024 * 1024 * 100) {
+                log.warn("文件大小超过100M，请选择一个较小的视频文件上传");
+                return ResponseEntity.badRequest().body("文件大小超过100M，请选择一个较小的视频文件上传");
+            }
+
+            String newFilename = videoService.upload(file);
+
+            log.info("文件上传成功，文件名：{}", newFilename);
+            return ResponseEntity.ok("文件上传成功，文件名：" + newFilename);
+        } catch (Exception e) {
+            log.error("文件上传失败", e);
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } finally {
+            semaphore.release();
         }
-
-        String newFilename = videoService.upload(file);
-
-        log.info("文件上传成功，文件名：{}", newFilename);
-        return ResponseEntity.ok("文件上传成功，文件名：" + newFilename);
     }
 }
